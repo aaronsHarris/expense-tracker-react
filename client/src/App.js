7c7725897618fb15f093e4b2de0166587e732c27
Aaron Harris <aaronsharris@gmail.com>
2022-01-06 13:07:50 -0600

    added addTransactionList and TransactionList components, cleaned up syntax errors on various components

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import { Header } from './components/Header.jsx'
 import {Balance} from './components/Balance.jsx'
+import {IncomeExpenses} from './components/IncomeExpenses.jsx'
+import { TransactionList } from './components/TransactionList';
+
 function App() {
   return (
     <div>
       <Header />
       <div className="container">
-      <Balance />
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
       </div>
     </div>
   );
