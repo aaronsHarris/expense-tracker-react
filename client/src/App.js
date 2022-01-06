4bf53729bdee7589266c086638f325008e96a279
Aaron Harris <aaronsharris@gmail.com>
2022-01-06 12:42:57 -0600

    added header and balance component, added global styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { Header } from './components/Header.jsx'
+import {Balance} from './components/Balance.jsx'
 function App() {
   return (
-    <div className="App">
-      
-      
+    <div>
+      <Header />
+      <div className="container">
+      <Balance />
+      </div>
     </div>
   );
 }
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Header = () => {
+    return (
+        <div>
+            expense tracker
+        </div>
+    )
+}
